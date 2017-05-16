79a2457eb858cb9b4eec56cf3aef3dc81872575a
Andrea <azdr0001@monash.edu>
2017-05-16 13:55:21 +1000

    copy pasted sstm file into arrow task

--- ArrowWM_analysis.m
+++ ArrowWM_analysis.m
@@ -1,1 +1,49 @@
 %Arrow task
+% Analaysis of SSTM working memory task
+% Copywrite James Morrow & Andrea Zdral 2017
+
+pathout = '\\ad.monash.edu\home\User077\morrowj\Desktop\Andrea_data\';
+
+subject = [ID{i,1},'_SSTM.txt'];
+a = dlmread(subject, '', 36, 0); %read in .txt file from line 37 downwards
+totalCorr_SSTM = sum(a(:,8)==2)./120*100; % calculates number of trials (out of 120) that were correct
+    
+for z = 1:30
+    [x,y]=find([a(:,9)]==z);
+    
+    if a(x,8)==2
+           corrAns(z) = 1;
+    else 
+           corrAns(z) = 0;
+    end;
+    
+    totalANU_SSTM = sum(corrAns(1,:)==1)./30*100;
+    
+end;
+    
+for idx = 1:30;
+    logA = a(:,9) == idx; %extract trial of interest
+    rowInt = a(:,8);
+    outputA = rowInt(logA);
+
+    logOutput = outputA == 2;
+    answer(idx,1) = sum(logOutput)./length(logOutput);
+    totalPCU_SSTM = sum(answer)./30*100;
+    end;
+    
+for z = 1:30
+   [x,y]=find([a(:,9)]==z);
+    
+   if a(x,8)==2
+           Ans(z) = 1*length(x);
+   else 
+           Ans(z) = 0;
+   end;
+    
+   totalANL_SSTM = sum(Ans)./120*100;
+    
+    end;
+
+output.(ID{1,1}) = struct('total_SSTM',{totalCorr_SSTM,totalANU_SSTM,totalPCU_SSTM,totalANL_SSTM});
+
+save([pathout,ID{i,1},'_output_SSTM'],'output');
