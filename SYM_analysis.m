b2deb4ae5633ff81a8f1424c4cc9ecd885c5847f
morrowjames <morrowj@monash.edu>
2017-05-16 11:57:04 +1000

    Commenting script

--- SYM_analysis.m
+++ SYM_analysis.m
@@ -4,13 +4,13 @@ ID = {'Sub_01';};
 
 for i = 1:size(ID,1)
     subject = [ID{i,1},'_SYM.xlsx'];
-    b = importdata(subject);
+    b = importdata(subject); % import .xlsx file
 end;
 
-w = b.textdata(:,25);
-w = w(~cellfun('isempty',w));
-w(1,:)=[];
+w = b.textdata(:,25); % remove irrelevant data fields and keep column 25
+w = w(~cellfun('isempty',w)); % if any cells are empty remove them
+w(1,:)=[]; % remove first cell as its a header and isnt needed
 
 s = 'success';
 
-tf = strcmp(w,s);
+tf = strcmp(w,s); % true/false. If cell = 'succes', print '1', else '0'.
